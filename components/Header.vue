9494f49464041440ef384bd7b53c40845f1ed1e2
miftahpj <142276212+miftahpj@users.noreply.github.com>
2024-02-26 15:52:44 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="row p-3 bg-primary text-white">
+    <div class="col-lg-1">
+    <img src="/asset/img/SMKN4.png" alt="logo SMKN 4">
+    </div>
+
+    <div class="col-lg-11">
+      <h1> Perpus Digital</h1>
+      <address>
+        SMKN 4 Tasikmalaya
+        Jl.Depok, Sukamenak, purbaratu
+      </address>
+    </div>
+  </div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container">
+    <Header />
+    <slot />
+  </div>
+</template>
